2e10a6016f7fc8b5bcfbe6ded48a52790de545e4
Rob Dawley <adawley@gmail.com>
2015-09-23 20:52:17 -0400

    updated formatting, added comments

--- src/components.js
+++ src/components.js
@@ -23,11 +23,16 @@ var components = {
 
             var makeLink = function(article) {
                 console.log(article);
-                var url = filters.url.hostname(article.url);
 
-                el.append('<p>')
+                var url = filters.url.hostname(article.url),
+                    pel = document.createElement('p');
+
+                $(pel)
+                  .addClass('article')
                   .append('<a href="' + article.url + '" target="_blank" class="article-title">' + article.title + '</a>')
-                  .append('<br/><span class="article-url">(' + url + ')</span></p>');
+                  .append('<br/><span class="article-url">(' + url + ') | <a href="https://news.ycombinator.com/item?id='+article.id+'">'+article.descendants+' comments</a></span>');
+
+                el.append(pel);
             };
 
             el.text(' ');
