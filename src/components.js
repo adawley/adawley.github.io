9f7410481f0e63682242a41544a58dbd7c54c93b
Rob Dawley <adawley@gmail.com>
2015-09-28 23:21:28 -0400

    updated to a new component block scheme

--- src/components.js
+++ src/components.js
@@ -1,25 +1,22 @@
 var components = {
 
     finviz: {
-        charts: function() {
-            var symbols = ['spy', 'fb'],
-                fv = services.finviz,
-                el = $('#finviz');
+        charts: function(el) {
+            var symbols = ['spy', 'fb', 'vglt'],
+                fv = services.finviz;
 
             el.text(' ');
 
             _.each(symbols, function(symbol) {
-                el.append(fv.getChart(symbol))
-                    .append(document.createElement('br'));
+                el.append(fv.getChart(symbol));
             });
 
         }
     },
 
     hacker_news: {
-        topStories: function() {
-            var hn = services.hacker_news,
-                el = $('#hacker_news');
+        topStories: function(el) {
+            var hn = services.hacker_news;
 
             var makeLink = function(article) {
                 console.log(article);
@@ -30,7 +27,7 @@ var components = {
                 $(pel)
                   .addClass('article')
                   .append('<a href="' + article.url + '" target="_blank" class="article-title">' + article.title + '</a>')
-                  .append('<br/><span class="article-url">(' + url + ') | <a href="https://news.ycombinator.com/item?id='+article.id+'">'+article.descendants+' comments</a></span>');
+                  .append('<br/><span class="article-url">(' + url + ') | <a href="https://news.ycombinator.com/item?id='+article.id+'" target="_blank">'+article.descendants+' comments</a></span>');
 
                 el.append(pel);
             };
